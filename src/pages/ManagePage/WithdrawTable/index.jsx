767d10b5c9931e95c02234b83f90216a68f81fc9
cotrang20012 <54300415+cotrang20012@users.noreply.github.com>
2022-11-15 18:36:37 +0700

    update withdraw table

--- src/pages/ManagePage/WithdrawTable/index.jsx
+++ src/pages/ManagePage/WithdrawTable/index.jsx
@@ -1,11 +1,13 @@
 import SearchIcon from '@mui/icons-material/Search';
-import { Box, Button, Divider, InputAdornment, Pagination, Paper, Stack, TextField } from '@mui/material';
+import { Box, Button, Divider, InputAdornment, Pagination, Paper, Stack, TextField, Chip } from '@mui/material';
 import 'assets/style.scss';
 import variables from 'assets/_variable.scss';
 import './style.scss';
 import WithdrawItem from './WithdrawItem';
+import * as React from 'react';
 
 function WithdrawTable() {
+	const [status, setStatus] = React.useState('ALL')
 	return (
 		<Box className="withdrawtable-container">
 			<Paper elevation={3} sx={{ width: '100%', height: '100%' }}>
@@ -26,7 +28,15 @@ function WithdrawTable() {
 						<Button variant="contained">TÌM KIẾM</Button>
 					</Stack>
 					<Divider />
+					<Stack direction={'row'} spacing={1}>
+					<Chip label="TẤT CẢ"  sx={{bgcolor:variables.mainlightercolor, color:'white',fontWeight:'bold'}}/>
+					<Chip label="CHẤP NHẬN" sx={{bgcolor:variables.textgreencolor, color:'white',fontWeight:'bold'}}  />
+					<Chip label="CHỜ DUYỆT" sx={{bgcolor:variables.orangecolor, color:'white',fontWeight:'bold'}} />
+					<Chip label="TỪ CHỐI"  sx={{bgcolor:variables.redcolor, color:'white',fontWeight:'bold'}}/>
+					</Stack>
                     <WithdrawItem/>
+					<WithdrawItem/>
+					<WithdrawItem/>
 					<Pagination count={10} shape="rounded" sx={{ alignSelf: 'center', justifySelf: 'flex-end' }} />
 				</Stack>
 			</Paper>
